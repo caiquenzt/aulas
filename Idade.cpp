f523d65c00ff03f729f2c200566491bd8b395dff
caiquenzt <99035515+caiquenzt@users.noreply.github.com>
2022-04-08 11:56:29 -0300

    Add files via upload

---
+++ Idade.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int numeroEsc, quadrado, cubo ;
+cout <<"Escolha um numero: " ;
+cin >> numeroEsc
+quadrado=numeroEsc*numeroEsc
+cubo=numeroEsc*numeroEsc*numeroEsc
+cout <<"Quadrado= " quadrado " e o cubo = "
+}
